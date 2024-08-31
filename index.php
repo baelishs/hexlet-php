1a5803ae739414823b71a812675eeecb17166cc3
baelishs <146851688+baelishs@users.noreply.github.com>
2024-08-31 18:12:11 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
+// Путь который будет использован при глобальной установке пакета
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+// Путь для локальной работы с проектом
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
 
 // Файл не включается на прямую
 // Он загрузится автоматически благодаря автозагрузке
