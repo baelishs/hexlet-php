7b2d62da719fc01af568d27120c30378500c7a5a
baelish <aaz8484@gmail.com>
2024-08-31 17:45:53 +0500

    add index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
+
